56102828cb75c316cda5976b0f1b040661fcf4cc
Leoni <leoni.dessau@mailbox.org>
2024-11-15 15:42:21 +0100

    more debug

--- irrad_control/utils/events.py
+++ irrad_control/utils/events.py
@@ -87,7 +87,7 @@ def create_irrad_events():
         @classmethod
         def beam_ok(cls):
             #----------------------------------- Debug stuff
-            _ = [logging.critical(str(ev.value.is_valid())) for ev in cls.beam_events()]
+            logging.critical(str(cls.beam_events().BeamOff.value))
             return not any(ev.value.is_valid() for ev in cls.beam_events())
 
         @classmethod
