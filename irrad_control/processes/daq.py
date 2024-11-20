9e3d9225274b0185467d3026ddc1bd4bf57186b3
Leoni <leoni.dessau@mailbox.org>
2024-11-20 13:12:01 +0100

    if i understand correctly this should be a SUB socket

--- irrad_control/processes/daq.py
+++ irrad_control/processes/daq.py
@@ -52,7 +52,7 @@ class DAQProcess(Process):
         # Ports/sockets used by this process
         self.ports = {'log': None, 'cmd': None, 'data': None, 'event': None}
         self.sockets = {'log': None, 'cmd': None, 'data': None, 'event': None}
-        self.socket_type = {'log': zmq.PUB, 'cmd': zmq.REP, 'data': zmq.PUB, 'event': zmq.PUB}
+        self.socket_type = {'log': zmq.PUB, 'cmd': zmq.REP, 'data': zmq.PUB, 'event': zmq.SUB}
 
         # Attribute holding zmq context
         self.context = None
