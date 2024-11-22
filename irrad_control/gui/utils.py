f43a9f18992f79d626356c8d470c51c46f3bf4b1
Leoni <leoni.dessau@mailbox.org>
2024-11-22 13:37:08 +0100

    host ip was being weird. it's dead now.

--- irrad_control/gui/utils.py
+++ irrad_control/gui/utils.py
@@ -39,7 +39,7 @@ def fill_combobox_items(cbx, fill_dict):
 
 def get_host_ip():
     """Returns the host IP address on UNIX systems. If not UNIX, returns None"""
-
+    return None
     try:
         host_ip = str(subprocess.check_output(['hostname', '-I']))
     except (OSError, subprocess.CalledProcessError):
